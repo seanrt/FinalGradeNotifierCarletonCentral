530b6b648ffc2155a689106b14bb62b4c2548673
Sean <seanrtohidi@gmail.com>
2016-12-29 19:08:31 -0500

    Cleaned up the code, updated the README

--- GradeChecker.py
+++ GradeChecker.py
@@ -1,37 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+from datetime import datetime
 
+TIME_DELAY_IN_MINUTES = 5
 loginUrl = 'https://central.carleton.ca/prod/twbkwbis.P_ValLogin'
 gradeUrl = 'https://central.carleton.ca/prod/bwskogrd.P_ViewGrde'
-TIME_DELAY_IN_MINUTES = 5
 
 loginData = {}
 termData = {'term_in': '201630'} # This corresponds to Fall 2016
 
+# This loop gathers login information and breaks once the login is successful
 while True:
     sid = input('Please input your student id (10XXXXXXX): ')
     pin = input('Please input your PIN: ')
     loginData['sid'] = sid
     loginData['PIN'] = pin
 
+    # We need a new session to store the cookies for Carleton Central
     s = requests.Session()
     loginPage = s.get(loginUrl)
     cookies = dict(loginPage.cookies)
     r = s.post(loginUrl, data=loginData)
     soup = BeautifulSoup(r.content, 'html.parser')
-    if 'Failure' not in soup.get_text():
-        print('Login Successful\n')
+    if 'Failure' not in soup.get_text(): # 'Failure' shows up in the failed login page
+        print('Login Successful')
         break
     print('Login Failed\n')
 
+# This loop continuously checks the final grades page, printing courses which have grades posted
 while True:
+    print('\n'+str(datetime.now().strftime('%H:%M:%S')))
     print('Courses with grades posted')
     r = s.post(gradeUrl, data=termData)
     soupTable = BeautifulSoup(r.content, 'html.parser').body.find('div', class_='pagebodydiv').find('table', class_='datadisplaytable').find_next_sibling('table')
     numTR = len(soupTable.find_all('tr'))
     result = soupTable.find('tr')
-    for i in range(1,numTR):
+    for i in range(1,numTR): # The first tr tag contains the grade table headers, so it is skipped
         result = result.find_next_sibling('tr')
         resultData = result.find_all('td')
         print(resultData[1].get_text()+resultData[2].get_text())
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FinalGradeNotifierCarletonCentral
+
+This script is for Carleton University students. It checks Carleton Central to see if final grades have been posted for a given semester.
+
+The user inputs their student number, their pin, and the semester they want to check. The script then continuously checks for and prints courses which have final grades posted.
+
+This script was written in Python 3 and uses the bs4 library (BeautifulSoup).
