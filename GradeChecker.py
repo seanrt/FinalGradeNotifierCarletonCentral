a1ab5147a54e628fdc0ce7ea51e5ef42ada0f168
seanrt <seanrtohidi@gmail.com>
2019-04-29 13:00:22 -0400

    Updated the script to work with carleton central changes. Cookies must be manually given.

--- GradeChecker.py
+++ GradeChecker.py
@@ -2,45 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 import time
 from datetime import datetime
-import getpass
 
 TIME_DELAY_IN_MINUTES = 5
-loginUrl = 'https://central.carleton.ca/prod/twbkwbis.P_ValLogin'
 gradeUrl = 'https://central.carleton.ca/prod/bwskogrd.P_ViewGrde'
+termData = {'term_in': 201910}
 
-loginData = {}
-termData = {'term_in': '201710'} # This corresponds to Winter 2017
+cookie_input = input()
+cookie = {'SESSID': cookie_input}
+soupTable = None
+s = requests.Session()
 
-# This loop gathers login information and breaks once the login is successful
 while True:
-    sid = input('Please input your student id (10XXXXXXX): ')
-    pin = getpass.getpass('Please input your PIN: ')
-    loginData['sid'] = sid
-    loginData['PIN'] = pin
-
-    # We need a new session to store the cookies for Carleton Central
-    s = requests.Session()
-    loginPage = s.get(loginUrl)
-    cookies = dict(loginPage.cookies)
-    r = s.post(loginUrl, data=loginData)
-    soup = BeautifulSoup(r.content, 'html.parser')
-    if 'Failure' not in soup.get_text(): # 'Failure' shows up in the failed login page
-        print('Login Successful')
-        break
-    print('Login Failed\n')
-
-# This loop continuously checks the final grades page, printing courses which have grades posted
-while True:
-    print('\n'+str(datetime.now().strftime('%H:%M:%S')))
-    print('Courses with grades posted')
-    r = s.post(gradeUrl, data=termData)
-    soupTable = BeautifulSoup(r.content, 'html.parser').body.find('div', class_='pagebodydiv').find('table', class_='datadisplaytable').find_next_sibling('table')
-    numTR = len(soupTable.find_all('tr'))
-    if numTR < 2:
+    print('*'*15)
+    print(str(datetime.now().strftime('%H:%M:%S')) + '\n')
+    r = s.post(gradeUrl, data=termData, cookies=cookie)
+    try:
+        soupTable = BeautifulSoup(r.content, 'html.parser').body.find_all('table')[6]
+    except IndexError:
         print('No grades yet')
-    result = soupTable.find('tr')
-    for i in range(1,numTR): # The first tr tag contains the grade table headers, so it is skipped
-        result = result.find_next_sibling('tr')
-        resultData = result.find_all('td')
-        print(resultData[1].get_text()+resultData[2].get_text())
+
+    if soupTable:
+        number_of_TR_elements = len(soupTable.find_all('tr')) - 1
+        currentRow = soupTable.find('tr')
+        for i in range(1, number_of_TR_elements + 1):
+            currentRow = currentRow.find_next('tr')
+            rowData = currentRow.find_all('td')
+            print(rowData[1].get_text()+rowData[2].get_text())
     time.sleep(TIME_DELAY_IN_MINUTES*60)
